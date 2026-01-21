41f013500063330f1b8ee454e86f1cb509a95831
gayathrig3063 <gayuthrish3063@gmail.com>
2026-01-21 17:58:28 +0530

    all work complete

---
+++ ply-script.js
@@ -0,0 +1,7 @@
+const MobView = document.getElementById("mob-view")
+MobView.style.display = "none"
+
+
+function showMenu() {
+    MobView.style.display = MobView.style.display === "none" ? 'block' : 'none'
+}
